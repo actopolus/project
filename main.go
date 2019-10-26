c9351c7dedd42be1d10264ed28bdeba5261c20bb
activist <bereznev@kolesa.kz>
2019-10-27 00:34:12 +0600

    master уменьшает количество кода

--- main.go
+++ main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"encoding/json"
+	"encoding/xml"
+	"errors"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/BurntSushi/toml"
-	"github.com/actopolus/project/api"
-	"github.com/actopolus/project/web"
 )
 
-// конфиг
+// Конфигурация
 type config struct {
-	Listen string
-	URL    api.URL
-	Http   struct {
-		Timeout string
-	}
+	Listen    string
+	Timeout   time.Duration
+	Countries string
+}
+
+// Обработчик запросов
+type Web struct {
+	cfg    config
+	client http.Client
+}
+
+// Структура ответа
+type Response struct {
+	Code  int         `xml:"code"`
+	Error string      `xml:"error,omitempty"`
+	Data  interface{} `xml:"data,omitempty"`
+}
+
+// Информация о стране
+type Country struct {
+	Name    string   `json:"nativeName" xml:"name"`
+	Flag    string   `json:"flag" xml:"flag"`
+	Borders []string `json:"borders" xml:"borders"`
+}
+
+// NewWeb - создаёт новый обработчик запросов
+func NewWeb(cfg config) Web {
+	return Web{cfg: cfg, client: http.Client{Timeout: cfg.Timeout * time.Millisecond}}
 }
 
 // main - точка входа
@@ -35,17 +62,69 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	apiReq, err := api.NewApi(cfg.Http.Timeout, cfg.URL)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	app := web.NewApp(apiReq)
-	http.HandleFunc("/info/", app.Info)
+	web := NewWeb(cfg)
+	http.HandleFunc("/", web.Handle)
 
 	log.Println("Listen and serve on", cfg.Listen)
 	if err := http.ListenAndServe(cfg.Listen, nil); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+// getCountry - получает название страны из URL
+func (w *Web) getCountry(url string) (country string, err error) {
+	pos := strings.LastIndexByte(url, '/')
+
+	if pos == -1 || pos == len(url) {
+		return country, errors.New(`Param "country" isn't valid`)
+	}
+
+	return url[pos+1:], nil
+}
+
+// sendError - выводит ошибку
+func (w *Web) sendError(rw http.ResponseWriter, statusCode int, err error) {
+	body, _ := xml.Marshal(Response{
+		Code:  1,
+		Error: err.Error(),
+	})
+
+	rw.WriteHeader(statusCode)
+	_, _ = rw.Write(body)
+}
+
+// Handle - отдаёт информацию о стране
+func (w *Web) Handle(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "application/xml")
+
+	country, err := w.getCountry(req.URL.Path)
+	if err != nil {
+		w.sendError(rw, http.StatusBadRequest, err)
+		return
+	}
+
+	resp, err := w.client.Get(w.cfg.Countries + country)
+	if err != nil {
+		w.sendError(rw, http.StatusInternalServerError, err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.sendError(rw, http.StatusInternalServerError, err)
+		return
+	}
+
+	var response []Country
+	if err := json.Unmarshal(b, &response); err != nil {
+		w.sendError(rw, http.StatusInternalServerError, err)
+		return
+	}
+
+	body, _ := xml.Marshal(response[0])
+
+	rw.WriteHeader(http.StatusOK)
+	_, _ = rw.Write(body)
 }
